61fec23fb379d77f1dbb2f8915c60c23cf122065
Mohit Yadav <mohit.yadav@deliveryhero.com>
2020-12-14 14:14:27 +0400

    update basic buttons and files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Modal, Form } from 'react-bootstrap';
 import './App.css';
 
+const ContentModal = ({ id, handleClose }: {id: string, handleClose: Function}) => (<Modal
+show
+onHide={handleClose}
+backdrop="static"
+keyboard={false}
+>
+<Modal.Header className="modal-header">
+<Button variant="info" size="lg" className="button-a-color">All Contacts</Button>{' '}
+<Button variant="info" size="lg" className="button-b-color">US Contacts</Button>{' '}
+<Button variant="info" size="lg" className="button-c" onClick={() => handleClose()}>Close</Button>
+</Modal.Header>
+<Modal.Body>
+
+</Modal.Body>
+<Modal.Footer className="modal-footer">
+  <Form.Check type="checkbox" label="Only even" />
+</Modal.Footer>
+</Modal>)
+
 function App() {
+  const [modal, setModal] = useState({
+    open: false,
+    id: ''
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="buttons-wrapper">
+        <Button variant="info" size="lg" className="button-a-color no-hover" onClick={() => setModal(v => ({...v, open: true, id: 'A'}))}>Button A</Button>{' '}
+        <hr />
+        <Button variant="info" size="lg" className="button-b-color no-hover" onClick={() => setModal(v => ({...v, open: true, id: 'B'}))}>Button B</Button>
+      </div>
+      {modal.open && <ContentModal id={modal.id} handleClose={() => setModal(v => ({...v, open: false}))} />}
     </div>
   );
 }
