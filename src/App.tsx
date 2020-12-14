bafb39574fe5ce74e1da547040a9a454e4b90d86
Mohit Y <42065586+Mohit21GoJs@users.noreply.github.com>
2020-12-14 16:06:43 +0400

    PR For Production Ready Features (#1)
    * update basic buttons and files
    
    * structure code
    
    * add errors for api layer
    
    Co-authored-by: Mohit Yadav <mohit.yadav@deliveryhero.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {  useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { fetchContacts } from './api/contacts';
+import { ContactListModal } from './components/ContactListModal'
+import ButtonA from './components/ButtonA';
+import ButtonB from './components/ButtonB';
+import { changeUrlHash, clearUrlHash } from './utils/url'
 import './App.css';
 
 function App() {
+  const [modal, setModal] = useState({
+    open: false,
+    id: ''
+  });
+  useEffect(() => {
+    if(modal.id){
+      changeUrlHash(`modal/${modal.id}`);
+      if(modal.id === "A"){
+        fetchContacts().then(console.log);
+      }
+    }else{
+      clearUrlHash();
+    }
+  }, [modal.id])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="buttons-wrapper">
+        <ButtonA handleClick={() => setModal(v => ({...v, open: true, id: 'A'}))}>Button A</ButtonA>{' '}
+        <hr />
+        <ButtonB handleClick={() => setModal(v => ({...v, open: true, id: 'B'}))}>Button B</ButtonB>
+      </div>
+      {modal.open && <ContactListModal id={modal.id} handleClose={() => setModal(v => ({...v, open: false, id: ''}))} />}
     </div>
   );
 }
